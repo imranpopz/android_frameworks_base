c9e0dcc891368fe1f0c3cc7224b4c2b374f1eaaf
jhonboy121 <alfredmathew05@gmail.com>
2022-02-01 19:54:15 +0530

    base: Introduce app lock [1/4]
    base: applock: refactor
    
    * encapsulate config data sources and add a data class for per app config
    * added support for migrating between different versions of data
    * fixed notification content not being redacted.
    
    services: applock: allow background authentication
    
    base: merge hidden app feature with applock
    
    services: AppLockConfig: fix a derp in hidePackage()
    
    services: applock: fix data migration and change secure notification parameter name
    
    * also fixed addPackage replacing data without checking if entry already exists
    
    services: AppLockManagerService: use the og calling uid for target intent
    
    * fixes launching app activites not exported
    
    * nift4: avoid changing public api
    
    * @neobuddy89: Add privapp permissions for Settings, updated for A14
    
    Change-Id: Ie35c6c7103b8bed644d510d8268d6c99cb71a1a2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Update aospa_strings.xml
    
    fix
    
    AppLock: Update API to hide unprotected apps
    
    * To allow hiding apps in launcher even if app not protected
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    AppLock: Allow locking all apps visible in launcher
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Update YaapUtils.java
    
    AppLock: Move services to new package
    
    Change-Id: I14ae3fe60a0b8f26e52d6e598f772d45c21d12db
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/SystemServiceRegistry.java
+++ core/java/android/app/SystemServiceRegistry.java
@@ -1607,6 +1607,29 @@ public final class SystemServiceRegistry {
                     }
                 });
 
+        registerService(Context.POCKET_SERVICE, PocketManager.class,
+                new CachedServiceFetcher<PocketManager>() {
+                    @Override
+                    public PocketManager createService(ContextImpl ctx)
+                            throws ServiceNotFoundException {
+                        IBinder binder = ServiceManager.getServiceOrThrow(
+                                Context.POCKET_SERVICE);
+                        IPocketService service =
+                                IPocketService.Stub.asInterface(binder);
+                        return new PocketManager(ctx.getOuterContext(), service);
+                    }});
+
+        registerService(Context.APP_LOCK_SERVICE, AppLockManager.class,
+                new CachedServiceFetcher<AppLockManager>() {
+                    @Override
+                    public AppLockManager createService(ContextImpl ctx)
+                            throws ServiceNotFoundException {
+                        IBinder binder = ServiceManager.getServiceOrThrow(
+                                Context.APP_LOCK_SERVICE);
+                        return new AppLockManager(ctx,
+                            IAppLockManagerService.Stub.asInterface(binder));
+                    }});
+
         sInitializing = true;
         try {
             // Note: the following functions need to be @SystemApis, once they become mainline
