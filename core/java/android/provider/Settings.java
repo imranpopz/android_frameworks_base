7755b715e73f6b9856bb15da4f6d90ad9caea626
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-12 12:54:47 +0800

    Volume Dialog: Add switch for media controller button [1/2]
    Change-Id: I58293a4bb5312e713d8919a1efa6fabc53146b51

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6053,6 +6053,11 @@ public final class Settings {
         @Readable
         public static final String NOTIFICATION_VIBRATION_PATTERN = "notification_vibration_pattern";
 
+	/**
+         * @hide
+         */
+        public static final String VOLUME_MEDIA_OUTPUT_TOGGLE = "volume_media_output_toggle";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
